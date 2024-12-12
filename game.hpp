ccfdf6c54db5e3ad5778b7a10dd7ff6d02ba1154
UndebuggableBug <191426186+UndebuggableBug@users.noreply.github.com>
2024-12-13 00:37:36 +0800

    0.2% completed

--- game.hpp
+++ game.hpp
@@ -2,82 +2,230 @@
 #define GAME_H
 
 #include <vector>
+#include <functional>
 
 namespace game {
+    typedef long long cashType;
+
     namespace gamePlay {        
         class Tile {
-            private:
             public:
-            // Tile type
-            // What should I write here?
             enum TileType {placeholder, home, buildable, prison, tax, random};
 
-            TileType type;
-            Tile(TileType _type = placeholder) : type(_type) {}
-            ~Tile() {}
-        };
-
-        class Buildable : Tile {
             private:
+            // Tile type
+            // What should I write here?
+            TileType type;
+
             public:
-            Buildable() : Tile(buildable) {}
-            ~Buildable() {}
+
+            Tile(TileType _type = placeholder) : type(_type) {}
+            virtual ~Tile() {}
+
+            TileType getType() const {
+                return type;
+            }
         };
 
-        class Home : Tile {
+        class Buildable : public Tile {
+            private:
+            cashType cost;
+            cashType rent;
+            bool owned;
+            player::Player* owner;
+
+            public:
+            Buildable(cashType _cost = 0, cashType _rent = 0) : Tile(buildable), cost(_cost), rent(_rent), owned(false), owner(nullptr) {}
+            ~Buildable() override {}
+
+            bool isOwned() const {
+                return owned;
+            }
+
+            player::Player* getOwner() const {
+                return owner;
+            }
+
+            void setOwner(player::Player* newOwner) {
+                owner = newOwner;
+            }
+
+            int getCost() const {
+                return cost;
+            }
+
+            int getRent() const {
+                return rent;
+            }
+
+            void setCost(int newCost) {
+                cost = newCost;
+            }
+
+            void setRent(int newRent) {
+                rent = newRent;
+            }
+        };
+
+        class Home : public Tile {
             private:
             public:
             Home() : Tile(home) {}
-            ~Home() {}
+            ~Home() override {}
         };
 
-        class Prison : Tile {
+        class Prison : public Tile {
             private:
             public:
             Prison() : Tile(prison) {}
-            ~Prison() {}
+            ~Prison() override {}
         };
 
-        class Tax : Tile {
+        class Tax : public Tile {
             private:
+            double taxRate;
+
             public:
-            Tax() : Tile(tax) {}
-            ~Tax() {}
+            Tax(double _rate = 0.0f) : Tile(tax), taxRate(_rate) {}
+            ~Tax() override {}
+
+            double getTaxRate() const {
+                return taxRate;
+            }
+
+            void setTaxRate(double newRate) {
+                taxRate = newRate;
+            }
         };
 
-        class Random : Tile {
+        class Random : public Tile {
             private:
             public:
             Random() : Tile(random) {};
-            ~Random() {}
+            ~Random() override {}
         };
 
-        class Map {
+        class GameInstance {
             private:
-            public:
-            std::vector<Tile> Tiles;
+            std::vector<Tile*> tiles;
+            std::vector<player::Player*> players;
+            int currentPlayerIndex;
 
-            Map() {}
-            ~Map() {}
+            public:
+            std::function<bool(player::Player* player, Buildable* tile)> callbackBuild = [](player::Player*, Buildable*)->bool{ return true; };
+            std::function<void(player::Player* player, Buildable* tile)> callbackRent = [](player::Player*, Buildable*)->void{};
+            std::function<void(player::Player* player, Buildable* tile)> callbackTax = [](player::Player*, Buildable*)->void{};
+
+            GameInstance() : currentPlayerIndex(0) {}
+            ~GameInstance() {}
+            
+            const std::vector<Tile*>& getTiles() const {
+                return tiles;
+            }
+
+            const std::vector<player::Player*>& getPlayers() const {
+                return players;
+            }
+            
+            void addTile(Tile* tile) {
+                tiles.push_back(tile);
+            }
+
+            void addPlayer(player::Player* player) {
+                players.push_back(player);
+            }
+
+            player::Player* getCurrentPlayer() const {
+                return players[currentPlayerIndex];
+            }
+
+            void nextPlayer() {
+                currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
+            }
+
+            void movePlayer(player::Player* player, int steps) {
+                int newPosition = (player->getPosition() + steps) % tiles.size();
+                player->setPosition(newPosition);
+                handleTileEvent(player, tiles[newPosition]);
+            }
+
+            void handleTileEvent(player::Player* player, Tile* tile) {
+                switch (tile->getType()) {
+                    case Tile::buildable: {
+                        Buildable* buildableTile = static_cast<Buildable*>(tile);
+                        if (!buildableTile->isOwned()) {
+                            // UI Callback event to be added
+                            if (callbackBuild(player, buildableTile)) {
+                                if (player->getCash() >= buildableTile->getCost()) {
+                                    player->setCash(player->getCash() - buildableTile->getCost());
+                                    buildableTile->setOwner(player);
+                                }
+                            }
+                        } else if (buildableTile->getOwner() != player) {
+                            // Pay rent
+                            player->setCash(player->getCash() - buildableTile->getRent());
+                            buildableTile->getOwner()->setCash(buildableTile->getOwner()->getCash() + buildableTile->getRent());
+                            callbackRent(player, buildableTile);
+                        }
+                        break;
+                    }
+                    case Tile::prison:
+                        break;
+                    case Tile::tax: {
+                        Tax* taxTile = static_cast<Tax*>(tile);
+                        //player->setCash(player->getCash() - taxTile->getTaxAmount());
+                        break;
+                    }
+                    case Tile::random:
+                        break;
+                    case Tile::home:
+                        break;
+                    default:
+                        break;
+                }
+            }
         };
     }
     
     namespace player {
         class Player {
             private:
-            public:
-            long long cash;
+            cashType cash;
+            int position;
 
-            Player(long long initialCash = 0) : cash(initialCash) {}
-            ~Player() {}
+            public:
+
+            Player(cashType initialCash = 0) : cash(initialCash), position(0) {}
+            virtual ~Player() {}
+
+            cashType getCash() {
+                return cash;
+            }
+
+            void setCash(cashType newCash) {
+                cash = newCash;
+            }
+
+            cashType addCash(cashType delta) {
+                cash += delta;
+                return cash;
+            }
+
+            int getPosition() const { 
+                return position; 
+            }
+
+            void setPosition(int pos) { 
+                position = pos; 
+            }
         };
 
         class ComputerPlayer : Player {
             private:
             public:
 
-            ComputerPlayer(long long initialCash = 0) : Player(initialCash) {}
-            ~ComputerPlayer() {}
+            ComputerPlayer(cashType initialCash = 0) : Player(initialCash) {}
+            ~ComputerPlayer() override {}
         };
     }
 }
