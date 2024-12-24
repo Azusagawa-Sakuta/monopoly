99b934e89a5232f0f765b3200fb2aaa61ff05b6d
UndebuggableBug <191426186+UndebuggableBug@users.noreply.github.com>
2024-12-24 14:00:23 +0800

    modified frontend

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1,9 +1,17 @@
 #include "startWidget.h"
 
 #include <QApplication>
+#include <thread>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+    std::thread([]() {
+        int playerCount = 4;
+        game::constant::homeReward = 2500;
+        game::constant::defaultTaxRate = 0.1f;
+        game::constant::initialCash = 10000;
+        game::gamePlay::GameInstance &g = game::gamePlay::GameInstance::getInstance();
+    }).detach();
+    
     QApplication a(argc, argv);
     Widget w;
     w.show();
